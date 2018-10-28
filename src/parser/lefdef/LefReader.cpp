7c324a2fe05c25e59c918472d69acc3991a1b8b5
Yi Zheng <zengngai@hust.edu.cn>
2018-10-27 20:46:43 -0500

    add parseCutLayer to Lef Reader

--- src/parser/lefdef/LefReader.cpp
+++ src/parser/lefdef/LefReader.cpp
@@ -424,6 +424,330 @@
 
     }
 
+    IndexType LefReader::parseCutLayer(lefiLayer const &v)
+    {
+        LefLayerCut cut = LefLayerCut();
+        cut.name = std::string(v.name());
+        cut.width = v.width();
+        IntType numSpacing = v.numSpacing();
+        /// TODO: Handle other spacings
+        for (IntType spacingIdx = 0; spacingIdx < numSpacing; ++ spacingIdx)
+        {
+            //cut.spacing = v.spacing(spacingIdx);
+            cut.spacing = v.spacing(0);
+        }
+        /// Return index of this in the cut layers
+        IndexType cutIdx = _techDB.cutLayers().size();
+        _techDB.cutLayers().emplace_back(cut);
+        return cutIdx;
+    }
+
+    IndexType LefReader::parseRoutingLayer(lefiLayer &v)
+    {
+        LefLayerRouting route = LefLayerRouting();
+
+        route.name = std::string(v.name());
+        if (v.hasPitch())
+        {
+            route.hasPitch = true;
+            route.pitch = v.pitch();
+        }
+        if (v.hasXYPitch())
+        {
+            route.hasXYPitch = true;
+            route.pitchX = v.pitchX();
+            route.pitchY = v.pitchY();
+        }
+        if (v.hasOffset())
+        {
+            route.hasOffset = true;
+            route.offset = v.offset();
+        }
+        if (v.hasXYOffset())
+        {
+            route.hasXYOffset = true;
+            route.offsetX = v.offsetX();
+            route.offsetY = v.offsetY();
+        }
+        if (v.hasWidth())
+        {
+            route.hasWidth = true;
+            route.width = v.width();
+        }
+        if (v.hasArea())
+        {
+            route.hasArea = true;
+            route.area = v.area();
+        }
+        if (v.hasSpacingNumber())
+        {
+            route.hasSpacingNumber = true;
+            route.numSpacing = v.numSpacing();
+        }
+        /// Process spacing information in other subroutine
+        for (IntType idx = 0; idx < route.numSpacing; ++idx)
+        {
+            processRoutingLayerSpacing(route, v, idx);
+        }
+
+        if (v.hasDirection())
+        {
+            route.hasDirection = true;
+            route.direction = std::string(v.direction());
+        }
+        if (v.hasMinwidth())
+        {
+            route.hasMinWidth = true;
+            route.minWidth = v.minwidth();
+        }
+        if (v.hasMaxwidth())
+        {
+            route.hasMaxWidth = true;
+            route.maxWidth = v.maxwidth();
+        }
+
+        ///spacing table
+        route.numSpacingTable = v.numSpacingTable();
+
+        for (IntType idx = 0; idx < route.numSpacingTable; ++idx)
+        {
+            const auto &spTable = v.spacingTable(idx);
+            if (spTable->isParallel())
+            {
+                processRoutingLayerSpacingTableParallel(route, spTable);
+            }
+            else if (spTable->isInfluence())
+            {
+                AssertMsg(false, "%s: expect no influence \n", __FUNCTION__);
+            }
+            /// Spacingtable can only be influence, parallel, and twoWidth
+            else
+            {
+                for (IntType twoWidthIdx = 0; twoWidthIdx < spTable->twoWidths()->numWidth(); ++twoWidthIdx)
+                {
+                    DBG("%s: layer %s twoWidth %d/%d: %d\n", __FUNCTION__, v.name(), twoWidthIdx, spTable->twoWidths()->numWidth(), spTable->twoWidths()->width(twoWidthIdx));
+                }
+            }
+            /// twoWidth
+        }
+
+        ///Return the index of the input in the routing layers
+        IndexType routingIdx = _techDB.routingLayers().size();
+        _techDB.routingLayers().emplace_back(route);
+        return routingIdx;
+    }
+
+    void LefReader::processRoutingLayerSpacing(LefLayerRouting &route, lefiLayer const &v, IntType spacingIdx)
+    {
+        if (v.hasSpacingName(spacingIdx))
+        {
+        }
+        else if (v.hasSpacingLayerStack(spacingIdx))
+        {
+        }
+        else if (v.hasSpacingAdjacent(spacingIdx))
+        {
+        }
+        else if (v.hasSpacingCenterToCenter(spacingIdx))
+        {
+        }
+        /// add others later... if not forever...
+        else if (v.hasSpacingEndOfLine(spacingIdx))
+        {
+            route.eolSpacing.emplace_back(EOLSpacing(v.spacing(spacingIdx), v.spacingEolWidth(spacingIdx), v.spacingEolWithin(spacingIdx)));
+        }
+        /// others...
+        else
+        {
+            /// raw spacing...
+            route.spacing.emplace_back(v.spacing(spacingIdx));
+        }
+    }
+
+    void LefReader::processRoutingLayerSpacingTableParallel(LefLayerRouting &route, lefiSpacingTable const *v)
+    {
+        const lefiParallel *para = v->parallel();
+        for (IntType lenIdx = 0; lenIdx < para->numLength(); ++lenIdx)
+        {
+            route.spacingTableParallelRunLength.emplace_back(para->length(lenIdx));
+        }
+        for (IntType widthIdx = 0; widthIdx < para->numWidth(); ++widthIdx)
+        {
+            route.spacingTableWidth.emplace_back(para->width(widthIdx));
+        }
+        route.spacingTableSpacing.resize(para->numLength(), para->numWidth());
+        for (IntType lenIdx = 0; lenIdx < para->numLength(); ++lenIdx)
+        {
+            for (IntType widthIdx = 0; widthIdx < para->numWidth(); ++widthIdx)
+            {
+                route.spacingTableSpacing.at(lenIdx, widthIdx) = para->widthSpacing(widthIdx, lenIdx);
+            }
+        }
+    }
+
+    IndexType LefReader::parseMastersliceLayer(const lefiLayer &v)
+    {
+        LefLayerMasterslice masterslice = LefLayerMasterslice();
+
+        masterslice.name = v.name();
+        IndexType masterIdx = _techDB.mastersliceLayers().size();
+        _techDB.mastersliceLayers().emplace_back(masterslice);
+        return masterIdx;
+    }
+
+    /// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    ///Via
+
+    void LefReader::parseFixedVia(const lefiVia &v)
+    {
+        LefRawFixedVia via;
+        via.name = v.name();
+        if (v.hasDefault())
+        {
+            via.isDefault = true;
+        }
+        if (v.hasResistance())
+        {
+            via.hasResistance = true;
+            via.resistance = v.resistance();
+        }
+
+        /// bottom, cut, top
+        AssertMsg(v.numLayers() == 3, "%s: VIA %s does not exactly have three layers, which is the default assumption. Instead, having %d \n", __FUNCTION__, v.name(), v.numLayers());
+
+        RealType xLo = 0;
+        RealType yLo = 0;
+        RealType xHi = 0;
+        RealType yHi = 0;
+
+        /// Bottom
+        via.bottomLayer = v.layerName(0);
+        AssertMsg(v.numRects(0) == 1, "%s: VIA %s bottom layer does not have exactly one rectangle! \n", __FUNCTION__, v.name());
+        AssertMsg(v.numPolygons(0) == 0, "%s, VIA %s bottom layer has polygons! \n", __FUNCTION__, v.name());
+        xLo = v.xl(0, 0);
+        yLo = v.yl(0, 0);
+        xHi = v.xh(0, 0);
+        yHi = v.yh(0, 0);
+        via.bottomLayerRect = Box<RealType>(xLo, yLo, xHi, yHi);
+
+        /// CUT
+        via.cutLayer = v.layerName(1);
+        AssertMsg(v.numRects(1) > 0, "%s: VIA %s cut layer does not have rectangle! \n", __FUNCTION__, v.name());
+        AssertMsg(v.numPolygons(1) == 0, "%s, VIA %s cut layer has polygons! \n", __FUNCTION__, v.name());
+        IntType numCut = v.numRects(1);
+        via.numCutRect = static_cast<IndexType>(numCut);
+        via.cutLayerRectArray.resize(0);
+        via.cutLayerRectArray.reserve(numCut);
+        for (IntType cutRectIdx = 0; cutRectIdx < numCut; ++cutRectIdx)
+        {
+            xLo = v.xl(1, cutRectIdx);
+            yLo = v.yl(1, cutRectIdx);
+            xHi = v.xh(1, cutRectIdx);
+            yHi = v.yh(1, cutRectIdx);
+            via.cutLayerRectArray.emplace_back(Box<RealType>(xLo, yLo, xHi, yHi));
+        }
+
+        /// top
+        via.topLayer = v.layerName(2);
+        AssertMsg(v.numRects(2) == 1, "%s: VIA %s top layer does not have exactly one rectangle! \n", __FUNCTION__, v.name());
+        AssertMsg(v.numPolygons(2) == 0, "%s, VIA %s top layer has polygons! \n", __FUNCTION__, v.name());
+        xLo = v.xl(2, 0);
+        yLo = v.yl(2, 0);
+        xHi = v.xh(2, 0);
+        yHi = v.yh(2, 0);
+        via.topLayerRect = Box<RealType>(xLo, yLo, xHi, yHi);
+
+        _rawFixedViaArray.emplace_back(via);
+    }
+
+    ////////////////////
+    /// Post processing
+    ////////////////////
+
+    /// Wrapper function
+    void LefReader::postProcessing()
+    {
+        /// Fixed VIA
+        _macroDB.fixedViaVec2D().resize(_techDB.accessLayerTypeVec().size());
+        for (const auto &rawVia : _rawFixedViaArray)
+        {
+            postProcessFixedVia(rawVia);
+        }
+        /// Sort Vias to put the default vias in the end
+        auto func = [&](LefFixedVia via1, LefFixedVia via2)
+        {
+            if (via1.isDefault())
+                return true;
+            else if (via2.isDefault())
+                return false;
+            else
+                return true;
+        };
+        for (auto &fixedViaVec : _macroDB.fixedViaVec2D())
+        {
+            std::stable_sort(fixedViaVec.begin(), fixedViaVec.end(), func);
+            for (auto &fixedVia : fixedViaVec)
+            {
+                fixedVia.findBoundingBox();
+            }
+        }
+    }
+
+    void LefReader::postProcessFixedVia(const LefRawFixedVia &rawVia)
+    {
+        LefFixedVia via;
+        via.setName(rawVia.name);
+        via.setIsDefault(rawVia.isDefault);
+        via.setHasResistance(rawVia.hasResistance);
+        via.setResistance(klib::round2Int(RESISTANCE_PRECISION * rawVia.resistance));
+
+        /// Get the correct index based on name
+        AssertMsg(_techDB.mapLayerStr2AccessIdx().find(rawVia.bottomLayer) != _techDB.mapLayerStr2AccessIdx().end(), "%s: via %s bottom %s top %s \n", __FUNCTION__, rawVia.name.c_str(), rawVia.bottomLayer.c_str(), rawVia.topLayer.c_str());
+        AssertMsg(_techDB.mapLayerStr2AccessIdx().find(rawVia.topLayer) != _techDB.mapLayerStr2AccessIdx().end() || \
+                _techDB.layerTypeVec()[_techDB.mapLayerStr2Idx()[rawVia.topLayer]].first == 3, "%s: via %s bottom %s top %s \n", __FUNCTION__, rawVia.name.c_str(), rawVia.bottomLayer.c_str(), rawVia.topLayer.c_str());
+        AssertMsg(_techDB.mapLayerStr2AccessIdx()[rawVia.topLayer] - _techDB.mapLayerStr2AccessIdx()[rawVia.bottomLayer] == 1, "%s: via %s bottom %s top %s \n", __FUNCTION__, rawVia.name.c_str(), rawVia.bottomLayer.c_str(), rawVia.topLayer.c_str());
+        via.setAccessLayerIdx(_techDB.mapLayerStr2AccessIdx()[rawVia.bottomLayer]);
+        via.setBottomLayerIdx(_techDB.mapLayerStr2Idx()[rawVia.bottomLayer]);
+        via.setCutLayerIdx(_techDB.mapLayerStr2Idx()[rawVia.cutLayer]);
+        via.setTopLayerIdx(_techDB.mapLayerStr2Idx()[rawVia.topLayer]);
+
+        /// Convert rect unit from um to databse unit
+        LocType xLo;
+        LocType yLo;
+        LocType xHi;
+        LocType yHi;
+
+        /// Bottom
+        xLo = static_cast<LocType>(rawVia.bottomLayerRect.xLo() * _techDB.units().databaseNumber);
+        yLo = static_cast<LocType>(rawVia.bottomLayerRect.yLo() * _techDB.units().databaseNumber);
+        xHi = static_cast<LocType>(rawVia.bottomLayerRect.xHi() * _techDB.units().databaseNumber);
+        yHi = static_cast<LocType>(rawVia.bottomLayerRect.yHi() * _techDB.units().databaseNumber);
+        via.bottomRect() = Box<LocType>(xLo, yLo, xHi, yHi);
+
+        /// Cut
+        via.setNumCutRect(rawVia.numCutRect);
+        via.cutRectArray().resize(0);
+        via.cutRectArray().reserve(rawVia.numCutRect);
+        for (IndexType cutIdx = 0; cutIdx < via.numCutRect(); ++cutIdx)
+        {
+            xLo = static_cast<LocType>(rawVia.cutLayerRectArray.at(cutIdx).xLo() * _techDB.units().databaseNumber);
+            yLo = static_cast<LocType>(rawVia.cutLayerRectArray.at(cutIdx).yLo() * _techDB.units().databaseNumber);
+            xHi = static_cast<LocType>(rawVia.cutLayerRectArray.at(cutIdx).xHi() * _techDB.units().databaseNumber);
+            yHi = static_cast<LocType>(rawVia.cutLayerRectArray.at(cutIdx).yHi() * _techDB.units().databaseNumber);
+            via.cutRectArray().emplace_back(Box<LocType>(xLo, yLo, xHi, yHi));
+        }
+
+        /// Top
+        xLo = static_cast<LocType>(rawVia.topLayerRect.xLo() * _techDB.units().databaseNumber);
+        yLo = static_cast<LocType>(rawVia.topLayerRect.yLo() * _techDB.units().databaseNumber);
+        xHi = static_cast<LocType>(rawVia.topLayerRect.xHi() * _techDB.units().databaseNumber);
+        yHi = static_cast<LocType>(rawVia.topLayerRect.yHi() * _techDB.units().databaseNumber);
+        via.topRect() = Box<LocType>(xLo, yLo, xHi, yHi);
+
+        /// push_back to the vector
+        _macroDB.fixedViaVec2D().at(via.accessLayerIdx()).emplace_back(via);
+    }
+
     void readLef(std::string const &fileName, MacroDataBase &db) {
         LefReader reader = LefReader(db);
         LefParser::read(reader, fileName.c_str());
